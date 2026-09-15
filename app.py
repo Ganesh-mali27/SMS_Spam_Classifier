e38a9ac507dcd6e30969430adbf66028887e1b53
Ganesh-mali27 <ganeshmali2784@gmail.com>
2026-09-15 21:36:18 +0530

    updated app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
